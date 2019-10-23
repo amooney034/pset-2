94c8dac99602a74332aac8131489fa455d78879d
Aidan Mooney <amooney034@gmail.com>
2019-10-23 10:49:57 -0400

    diagonal.js

---
+++ src/diagonal.js
@@ -0,0 +1,7 @@
+const readlineSync = require("readline-sync");
+let w_inches = Number(readlineSync.question("Width: "));
+let l_inches = Number(readlineSync.question("Length: "));
+let diagonal_inches = Math.sqrt((w_inches * w_inches) + (l_inches * l_inches));
+diagonal = Math.round(100*diagonal_inches)/(100);
+final_diagonal = diagonal.toLocaleString ()
+console.log("\nA(n) " + w_inches + "-by-" + l_inches + "-inch sheet of paper has a diagonal of " + final_diagonal + " inch(es).\n");
