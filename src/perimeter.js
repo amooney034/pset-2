4458b496a67735a457808496d68d9b0b8c172472
Aidan Mooney <amooney034@gmail.com>
2019-10-23 10:53:36 -0400

    perimeter.js

--- src/perimeter.js
+++ src/perimeter.js
@@ -1,6 +1,6 @@
 const readlineSync = require("readline-sync");
 const convert = 2.54
-let w_inches = Number(readlineSync.question("Width: "));
+let w_inches = Number(readlineSync.question("\nWidth: "));
 let l_inches = Number(readlineSync.question("Length: "));
 let width = w_inches * convert
 let length = l_inches * convert
