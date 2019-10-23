bcff102f3c7d95e647ded4aa92a68520e2244628
Aidan Mooney <amooney034@gmail.com>
2019-10-23 10:48:37 -0400

    area.js

--- src/area.js
+++ src/area.js
@@ -1,4 +1,5 @@
 const readlineSync = require("readline-sync");
+
 const convert = 25.4
 let w_inches = Number(readlineSync.question("Width: "));
 let l_inches = Number(readlineSync.question("Length: "));
@@ -7,4 +8,4 @@ let length = l_inches * 25.4
 let area = width * length
 area = Math.round(100*area)/(100);
 final_area = area.toLocaleString ();
-console.log("\nA(n) " + w_inches + "-by-" + l_inches + "- inch sheet of paper has an area of " + final_area + " square millimeter(s).\n");
+console.log("\nA(n) " + w_inches + "-by-" + l_inches + "-inch sheet of paper has an area of " + final_area + " square millimeter(s).\n");
