7c3a169da0171848cfc2daeac23890158ddcafed
Aidan Mooney <amooney034@gmail.com>
2019-10-25 11:56:30 -0400

    Friday Final

--- src/salary.js
+++ src/salary.js
@@ -1,9 +1,13 @@
 const readlineSync = require("readline-sync");
-const fourOoneK = 0.07
-const fed_income = 0.157
-const state_income = 0.0447
-const ss = 0.062
-const med = 0.0145
+const fourOoneK = 0.93
+const fed_income = 0.843
+const state_income = 0.9553
+const ss = 0.938
+const med = 0.9855
 const paycheck = 24
-let annual_salary = Number(readlineSync.question("\n"));
-let paid = (annual_salary / paycheck)
+let annual_salary = Number(readlineSync.question("\nAnnual salary: "));
+let paid = (annual_salary * fourOoneK)
+let second_paid = (paid * fed_income * state_income * ss * med);
+let final_paid = (second_paid / paycheck)
+let final_paycheck = final_paid.toLocaleString ("en", {minimumFractionDigits:2, maximumFractionDigits:2})
+console.log("\nYour take-home pay each check will be $" + final_paycheck)
