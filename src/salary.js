068d28c365994e103b88291ecfc532aa345c36a4
amooney034 <55448881+amooney034@users.noreply.github.com>
2019-10-27 18:30:08 -0400

    final sunday

--- src/salary.js
+++ src/salary.js
@@ -1,22 +1,24 @@
 const readlineSync = require("readline-sync");
 
+const annual_salary = Number(readlineSync.question("\nAnnual salary: "));
+
 const fed_income = 0.157
 const state_income = 0.0447
 const ss = 0.062
 const med = 0.0145
-const paycheck_times = 24
-const fourOoneK = 0.93
+const paycheck_times = annual_salary/24
+const fourOoneK = 0.07*(paycheck_times)
 
-let annual_salary = Number(readlineSync.question("\nAnnual salary: "));
-let paycheck = (annual_salary / paycheck_times);
-let contribution = (paycheck * fourOoneK);
-let pre_tax = (paycheck - contribution);
-let fed = (pre_tax * fed_income);
-let state = (pre_tax * state_income);
-let social = (pre_tax * ss);
-let medical = (pre_tax * med);
+let fourOoneKcheck = paycheck_times-fourOoneK
+
+
+let fed = (fed_income * fourOoneKcheck);
+let state = (state_income * fourOoneKcheck);
+let social = (ss * fourOoneKcheck)
+let medical = (med * fourOoneKcheck);
 let final_paid = (fed + state + social + medical);
-let final_paycheck = final_paid.toLocaleString ("en", {minimumFractionDigits:2, maximumFractionDigits:2});
+let final_paycheck = (fourOoneKcheck-final_paid).toLocaleString ("en", {minimumFractionDigits:2, maximumFractionDigits:2})
 
 console.log("\nYour take-home pay each check will be $" + final_paycheck + ".\n");
+
 
