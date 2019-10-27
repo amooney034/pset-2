804bd5f6c3fa98b83a01f00c8a68914ffd7c0f82
amooney034 <55448881+amooney034@users.noreply.github.com>
2019-10-27 18:18:28 -0400

    Update salary.js

--- src/salary.js
+++ src/salary.js
@@ -13,10 +13,10 @@ let contribution = (paycheck * fourOoneK);
 let pre_tax = (paycheck - contribution);
 let fed = (pre_tax * fed_income);
 let state = (pre_tax * state_income);
-let social = (pre_tax * ss)
+let social = (pre_tax * ss);
 let medical = (pre_tax * med);
 let final_paid = (fed + state + social + medical);
-let final_paycheck = final_paid.toLocaleString ("en", {minimumFractionDigits:2, maximumFractionDigits:2})
+let final_paycheck = final_paid.toLocaleString ("en", {minimumFractionDigits:2, maximumFractionDigits:2});
 
 console.log("\nYour take-home pay each check will be $" + final_paycheck + ".\n");
 
