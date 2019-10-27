0f6306d23348fe80d34295edeb22547a7a8e6abf
amooney034 <55448881+amooney034@users.noreply.github.com>
2019-10-27 18:16:50 -0400

    Update hourly.js

--- src/hourly.js
+++ src/hourly.js
@@ -1,12 +1,15 @@
 const readlineSync = require("readline-sync");
-const wage = Number(readlineSync.question("\nHourly wage: "));
-let monday = Number(readlineSync.question("\nMonday: "));
-let tuesday = Number(readlineSync.question("Tuesday: "));
-let wednesday = Number(readlineSync.question("Wednesday: "));
-let thursday = Number(readlineSync.question("Thursday: "));
-let friday = Number(readlineSync.question("Friday: "));
-let saturday = Number(readlineSync.question("Saturday: "));
-let sunday = Number(readlineSync.question("Sunday: "));
+
+const wage = readlineSync.question("\nHourly wage: ");
+
+let monday = readlineSync.question("\nMonday: ");
+let tuesday = readlineSync.question("Tuesday: ");
+let wednesday = readlineSync.question("Wednesday: ");
+let thursday = readlineSync.question("Thursday: ");
+let friday = readlineSync.question("Friday: ");
+let saturday = readlineSync.question("Saturday: ");
+let sunday = readlineSync.question("Sunday: ");
+
 let final_monday = (wage * monday);
 let final_tuesday = (wage * tuesday);
 let final_wednesday = (wage * wednesday);
@@ -14,7 +17,7 @@ let final_thursday = (wage * thursday);
 let final_friday = (wage * friday);
 let final_saturday = (wage * saturday);
 let final_sunday = (wage * sunday);
-let wage1 = (final_monday + final_tuesday + final_wednesday + final_thursday + final_friday + final_saturday + final_sunday)
-let final_wage = wage1.toLocaleString ("en", {minimumFractionDigits:2, maximumFractionDigits:2})
+let wage1 = (final_monday + final_tuesday + final_wednesday + final_thursday + final_friday + final_saturday + final_sunday);
+let final_wage = wage1.toLocaleString ("en", {minimumFractionDigits:2, maximumFractionDigits:2});
 
-console.log("\nYou’ll make $" + final_wage + " this week.\n")
+console.log("\nYou'll make " + "$"+ final_wage + " this week.\n");
